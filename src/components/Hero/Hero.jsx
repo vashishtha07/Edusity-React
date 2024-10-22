f5e1ecbb600befe2d5739eab0a82e82032928071
vashishtha07 <priyanshuvashishtha777@gmail.com>
2024-10-23 02:20:29 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+const Hero = () => {
+    return (
+        <div className='hero container'>
+            <div className="hero-text">
+                <h1>We ensure better education for a better world</h1>
+                <p>Our cutting edge currivulum is designed to empower sutdens with knowlede, skills , ans wxperiences needed to excel in thne dynamic field of education</p>
+                <button className="btn">Explore more<img src={dark_arrow} alt=""></img></button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
